0bd45206d9d2eb78c7003e79f9d896280f519a22
jirlock <jirlock@gmail.com>
2017-09-13 12:30:19 +0900

    wnet

--- wnet.py
+++ wnet.py
@@ -93,6 +93,7 @@ class Net(nn.Module):
 
 image,mask,tmask = pro.load_data_wnet()
 
+image.reshape(350,3,572,572).astype(np.float32)
 mask = mask.reshape(350,388,388,3).astype(np.float32)
 mask = np.swapaxes(mask,1,3)
 mask = np.swapaxes(mask,2,3)
