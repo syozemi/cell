a9f6fa05bc3397cbe20cd3b8554885cf5fef6fc8
jirlock <jirlock@gmail.com>
2017-09-14 12:58:59 +0900

    smaller unet

--- b.py
+++ b.py
@@ -12,6 +12,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from collections import defaultdict
 
 image,mask,tmask = pro.load_data_unet_torch2()
 
@@ -26,6 +27,15 @@ mask = mask[n,...]
 
 cc = mask[1,:,:]
 nn = mask[2,:,:]
+
+d = defaultdict(int)
+
+for x in cc:
+    for y in x:
+        d[y] += 1
+
+print(d)
+
 
 fig = plt.figure(figsize=(10,10))
 sub = fig.add_subplot(1,3,1)
