b4d38bb3abc6ca1133c85f28c48d536225fdc404
jirlock <jirlock@gmail.com>
2017-09-14 12:52:36 +0900

    smaller unet

--- b.py
+++ b.py
@@ -24,24 +24,16 @@ n = random.randint(0,350)
 image = image[n,...]
 mask = mask[n,...]
 
-i = image[0]
-c = image[1]
-n = image[2]
-
 cc = mask[:,:,1]
 nn = mask[:,:,2]
 
 fig = plt.figure(figsize=(10,10))
-sub = fig.add_subplot(3,2,1)
-sub.imshow(i,cmap='gray')
-sub = fig.add_subplot(3,2,3)
+sub = fig.add_subplot(1,3,1)
+sub.imshow(image,cmap='gray')
+sub = fig.add_subplot(1,3,2)
 sub.imshow(cc,cmap='gray')
-sub = fig.add_subplot(3,2,4)
+sub = fig.add_subplot(1,3,3)
 sub.imshow(nn,cmap='gray')
-sub = fig.add_subplot(3,2,5)
-sub.imshow(c,cmap='gray')
-sub = fig.add_subplot(3,2,6)
-sub.imshow(n,cmap='gray')
 plt.show()
 
 
