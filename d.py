c93b9ab55a9fa98d00b6dc87c9ef819908829e83
jirlock <jirlock@gmail.com>
2017-09-20 16:03:56 +0900

    a

---
+++ d.py
@@ -0,0 +1,118 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import process_data as pro
+import pickle
+import random
+from collections import defaultdict
+import torch_unet
+import matplotlib.pyplot as plt
+
+
+class Conv(nn.Module):
+    def __init__(self, ins, outs, activation=F.relu):
+        super(Conv,self).__init__()
+        self.conv1 = nn.Conv2d(ins,outs,3)
+        self.conv2 = nn.Conv2d(outs,outs,3)
+        self.activation = activation
+        self.norm = nn.BatchNorm2d(outs)
+
+    def forward(self,x):
+        out = self.activation(self.norm(self.conv1(x)))
+        out = self.activation(self.norm(self.conv2(out)))
+        return out
+
+class Up(nn.Module):
+    def __init__(self, ins, outs, activation=F.relu):
+        super(Up,self).__init__()
+        self.up = nn.ConvTranspose2d(ins,outs,2,stride=2)
+        self.conv1 = nn.Conv2d(ins,outs,3)
+        self.conv2 = nn.Conv2d(outs,outs,3)
+        self.activation = activation
+        self.norm = nn.BatchNorm2d(outs)
+
+    def crop(self, layer, target_size):
+        batch_size, n_channels, layer_width, layer_height = layer.size()
+        xy1 = (layer_width - target_size) // 2
+        return layer[:, :, xy1:(xy1 + target_size), xy1:(xy1 + target_size)]
+
+    def forward(self,x,bridge):
+        up = self.up(x)
+        crop = self.crop(bridge,up.size()[2])
+        out = torch.cat([up,crop],1)
+        out = self.activation(self.norm(self.conv1(out)))
+        out = self.activation(self.norm(self.conv2(out)))
+        return out
+
+class MulWeight(nn.Module):
+    def __init__(self, weight):
+        super(MulWeight, self).__init__()
+        self.w0 = weight[0]
+        self.w1 = weight[1]
+        self.w2 = weight[2]
+
+    def forward(self,x):
+        s0 = x[:,0,:,:]
+        s1 = x[:,1,:,:]
+        s2 = x[:,2,:,:]
+        s0 = torch.mul(s0,self.w0)
+        s1 = torch.mul(s1,self.w1)
+        s2 = torch.mul(s2,self.w2)
+        out = torch.stack([s0,s1,s2],1)
+        return out
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.activation = F.relu
+        self.conv_1_8 = Conv(1,8)
+        self.conv_8_16 = Conv(8,16)
+        self.conv_16_32 = Conv(16,32)
+        self.conv_32_64 = Conv(32,64)
+        self.conv_64_128 = Conv(64,128)
+        self.pool1 = nn.MaxPool2d(2)
+        self.pool2 = nn.MaxPool2d(2)
+        self.pool3 = nn.MaxPool2d(2)
+        self.pool4 = nn.MaxPool2d(2)
+        self.up1 = Up(128,64)
+        self.up2 = Up(64,32)
+        self.up3 = Up(32,16)
+        self.up4 = Up(16,8)
+        self.last = nn.Conv2d(8,3,1)
+        self.weight = MulWeight([0.8,1,1])
+
+    def forward(self, x):
+        block1 = self.conv_1_8(x)
+        pool1 = self.pool1(block1)
+
+        block2 = self.conv_8_16(pool1)
+        pool2 = self.pool2(block2)
+
+        block3 = self.conv_16_32(pool2)
+        pool3 = self.pool3(block3)
+
+        block4 = self.conv_32_64(pool3)
+        pool4 = self.pool4(block4)
+
+        bottom = self.conv_64_128(pool4)
+
+        up1 = self.up1(bottom,block4)
+
+        up2 = self.up2(up1,block3)
+
+        up3 = self.up3(up2,block2)
+
+        up4 = self.up4(up3,block1)
+
+        raw_score = self.last(up4)
+
+        score = self.weight(raw_score)
+
+        return F.softmax(score)
+
+
+torch_unet.eval(0)
+
