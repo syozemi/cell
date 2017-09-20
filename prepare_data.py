3bf7cdcaeb78f28271464a7eb020d3243aaa61bf
jirlock <jirlock@gmail.com>
2017-09-20 17:21:33 +0900

    a

--- prepare_data.py
+++ prepare_data.py
@@ -12,28 +12,29 @@ if not os.path.exists('data'):
 else:
     pass
 
-folders = os.listdir('picture/image')
-aug_type = os.listdir('picture/image/Band')
+folders = os.listdir('cell_data/image')
+#aug_type = os.listdir('picture/image/Band')
 for folder in folders:
-    for aug in aug_type:
+    #for aug in aug_type:
         try:
             image,mask,num_mask,ncratio,num_ncratio,num_ncratio10 = [],[],[],[],[],[]
-            files = os.listdir('picture/image/%s/%s' % (folder,aug))
+            #files = os.listdir('picture/image/%s/%s' % (folder,aug))
             for i,file in enumerate(files):
                 try:
                     #画像のパス
-                    ipath = 'picture/image/%s/%s/%s' % (folder,aug,file)
-                    cpath = 'picture/mask/%s/%s/%s' % (folder,aug,file.replace('.jpg', '.mask.0.png'))
-                    npath = 'picture/mask/%s/%s/%s' % (folder,aug,file.replace('.jpg', '.mask.1.png'))
-                    #ipath = 'cell_data/image/%s/%s' % (folder,file)
-                    #cpath = 'cell_data/mask/%s/%s' % (folder,file.replace('.jpg', '.mask.0.png'))
-                    #npath = 'cell_data/mask/%s/%s' % (folder,file.replace('.jpg', '.mask.1.png'))
+                    #ipath = 'picture/image/%s/%s/%s' % (folder,aug,file)
+                    #cpath = 'picture/mask/%s/%s/%s' % (folder,aug,file.replace('.jpg', '.mask.0.png'))
+                    #npath = 'picture/mask/%s/%s/%s' % (folder,aug,file.replace('.jpg', '.mask.1.png'))
+                    ipath = 'cell_data/image/%s/%s' % (folder,file)
+                    cpath = 'cell_data/mask/%s/%s' % (folder,file.replace('.jpg', '.mask.0.png'))
+                    npath = 'cell_data/mask/%s/%s' % (folder,file.replace('.jpg', '.mask.1.png'))
 
                     #画像をグレースケールで取得して正規化し拡大する
-                    img = cv.resize(cv.imread(ipath,0)/255,(572,572))
+                    #img = cv.resize(cv.imread(ipath,0)/255,(572,572))
+                    img = cv.imread(ipath,0)[3:,:]/255
 
                     #３クラスのマスクを作る
-                    msk, num_msk = pro.create_mask_label(cpath,npath,388)
+                    msk, num_msk = pro.create_mask_label(cpath,npath,360)
 
                     #マスクからnc比を計算する
                     ncr, num_ncr, num_ncr10 = pro.create_ncratio(msk)
@@ -59,12 +60,12 @@ for folder in folders:
             num_ncratio = np.array(num_ncratio)
             num_ncratio10 = np.array(num_ncratio10)
 
-            pro.save(image, 'data/%s/%s' % (folder,aug), 'image')
-            pro.save(mask, 'data/%s/%s' % (folder,aug), 'mask')
-            pro.save(num_mask, 'data/%s/%s' % (folder,aug), 'num_mask')
-            pro.save(ncratio, 'data/%s/%s' % (folder,aug), 'ncratio')
-            pro.save(num_ncratio, 'data/%s/%s' % (folder,aug), 'num_ncratio')
-            pro.save(num_ncratio10, 'data/%s/%s' % (folder,aug), 'num_ncratio10')
+            pro.save(image, 'data/%s' % folder, 'raw_image')
+            pro.save(mask, 'data/%s' % folder, 'raw_mask')
+            pro.save(num_mask, 'data/%s' % folder, 'raw_num_mask')
+            pro.save(ncratio, 'data/%s' % folder, 'raw_ncratio')
+            pro.save(num_ncratio, 'data/%s' % folder, 'raw_num_ncratio')
+            pro.save(num_ncratio10, 'data/%s' % folder, 'raw_num_ncratio10')
 
             print('%s/%s done' % (folder,aug))
                     
